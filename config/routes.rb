7abcd98041dbfa53e8ba117fab0f435e70f07a8d
Jeronim Morina <jeronim.morina@boostaxa.onmicrosoft.com>
2022-01-09 23:27:56 +0100

    feat: ✨ first crud steps with rails 7 😍

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "articles#index"
+
+  # get "/articles", to: "articles#index"
+  # get "articles/:id", to: "articles#show"
+  # replace get routes with universal resources to all CRUD routes for articles
+  resources :articles
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 end
